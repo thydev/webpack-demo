5aa71b2d25942953b023f198f6b6e5517f35f4bd
thydev <nginchanthy@gmail.com>
2018-06-18 22:57:07 -0700

    added images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from "lodash";
+import "./styles/style.css";
+import xtocat from "./images/xtocat.jpg";
 
 function component() {
   var element = document.createElement("div");
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our existing  div.
+  var myImg = new Image();
+  myImg.src = xtocat;
+  element.appendChild(myImg);
 
   return element;
 }

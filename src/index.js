e3c0cc99256a55ce20f8d59888051b51e18aa301
thydev <nginchanthy@gmail.com>
2018-06-18 23:51:27 -0700

    output management

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import _ from "lodash";
-import "./styles/style.css";
-import xtocat from "./images/xtocat.jpg";
-import Data from "./data/data.xml";
+import printMe from "./print.js";
 
 function component() {
   var element = document.createElement("div");
+  var btn = document.createElement("button");
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
 
-  // Add the image to our existing  div.
-  var myImg = new Image();
-  myImg.src = xtocat;
-  element.appendChild(myImg);
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
 
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
